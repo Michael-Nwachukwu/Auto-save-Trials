202d56698292f8cf11cb5fcfec35aa82ccbf84dc
AMD <michojekunle1@gmail.com>
2024-12-05 17:39:33 +0100

    implement approve erc20 token

--- src/components/CreatePlan.tsx
+++ src/components/CreatePlan.tsx
@@ -1,128 +1,108 @@
-"use client"
+"use client";
 
-// * @dev - This component should handle approving the total amount of tokens to beautosasved by the formula 
+// * @dev - This component should handle approving the total amount of tokens to beautosasved by the formula
 // * const totalAmount = amount * Math.floor(duration / frequency);
 // * Approve the ERC20 token
 // * Call the createAutomatedSavingsPlan function
-
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
-import { Button } from "@/components/ui/button"
-import {
-    Form,
-    FormControl,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import { parseUnits } from "viem"
-import { ethers } from "ethers"
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { erc20Abi } from "viem";
+import { useWriteContract } from "wagmi";
+import { waitForTransactionReceipt } from "@wagmi/core";
+import { config } from "@/wagmi";
+import { useState } from "react";
+import { Label } from "./ui/label";
 
 const tokenAddress = "0x40c56d5eaf26F014272941448280A33FF6F6e5D4";
-
-
-const FormSchema = z.object({
-    token: z.string().min(2, {
-        message: "token must be at least 2 characters.",
-    }),
-    amount: z.number().positive({
-        message: "token must be at least 2 characters.",
-    }),
-    frequency: z.number().positive({
-        message: "token must be at least 2 characters.",
-    }),
-    duration: z.number().positive({
-        message: "token must be at least 2 characters.",
-    }),
-})
+const contractAddress = "0xfd22a135213ee2F818845b91Cc8ab33e6B79F381";
 
 export function InputForm() {
-    const form = useForm<z.infer<typeof FormSchema>>({
-        resolver: zodResolver(FormSchema),
-        defaultValues: {
-            token: "",
-            amount: 0,
-            frequency: 0,
-            duration: 0,
-        },
-    })
+  const [amount, setAmount] = useState(0);
+  const [frequency, setFrequency] = useState(0);
+  const [duration, setDuration] = useState(0);
+  const { writeContractAsync } = useWriteContract();
 
-    async function onSubmit(data: z.infer<typeof FormSchema>) {
-        const { token, amount, frequency, duration } = data;
+  const handleCreateSavingsPlan = async () => {
+    if (!amount || !frequency || !duration)
+      return alert("Error, please fill in all form fields!");
 
-        const totalAmount = amount * Math.floor(duration / frequency);
+    const totalAmount = amount * Math.floor(duration / (frequency/(60*24)));
+    console.log(totalAmount);
 
-        // Approve the ERC20 token
-        try {
-            const tokenContract = new ethers.Contract(tokenAddress, ERC20_ABI, signer);
-            const tx = await tokenContract.approve("0xfd22a135213ee2F818845b91Cc8ab33e6B79F381", parseUnits(amount.toString(), 18));
-            await tx.wait();
-            console.log("Approval successful:", tx);
-        } catch (error) {
-            console.error("Approval failed:", error);
-        }
+    try {
+        const approveResponse = await writeContractAsync({
+            address: tokenAddress as `0x${string}`,
+            functionName: "approve",
+            abi: erc20Abi,
+            args: [contractAddress as `0x${string}`, BigInt(totalAmount * 10 ** 18)],
+          });
+      
+          // Check if the approve transaction was successful
+          if (approveResponse) {
+            console.log(`Approve transaction successful: ${approveResponse}`);
+      
+            // Step 2: Wait until the transaction is mined
+            const approveTransactionReceipt = await waitForTransactionReceipt(
+              config,
+              {
+                hash: approveResponse,
+              }
+            );
+      
+            console.log(approveTransactionReceipt);
+      
+            if (approveTransactionReceipt.status === "success") {
+              console.log(
+                "Approve transaction confirmed, proceeding with creating automated savings plan..."
+              );
+            }
+          }
+    } catch (error) {
+        alert(`An error occured ${error}`);
     }
 
-    return (
-        <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
-                <FormField
-                    control={form.control}
-                    name="token"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>token</FormLabel>
-                            <FormControl>
-                                <Input placeholder="shadcn" {...field} />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <FormField
-                    control={form.control}
-                    name="amount"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>token</FormLabel>
-                            <FormControl>
-                                <Input placeholder="shadcn" {...field} />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <FormField
-                    control={form.control}
-                    name="frequency"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>token</FormLabel>
-                            <FormControl>
-                                <Input placeholder="shadcn" {...field} />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <FormField
-                    control={form.control}
-                    name="duration"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>token</FormLabel>
-                            <FormControl>
-                                <Input placeholder="shadcn" {...field} />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <Button type="submit">Submit</Button>
-            </form>
-        </Form>
-    )
+    
+  };
+
+  return (
+    <div className="space-y-4 max-w-md w-full">
+      <div className="gap-2">
+        <Label>Token</Label>
+        <Input type="text" placeholder="Token" className="w-full" />
+      </div>
+      <div className="gap-2">
+        <Label>Duration</Label>
+        <Input
+          type="number"
+          value={duration}
+          onChange={(e) => setDuration(parseInt(e.target.value))}
+          placeholder="For How Long in days"
+          className="w-full"
+        />
+      </div>
+      <div className="gap-2">
+        <Label>Amount</Label>
+        <Input
+          type="number"
+          value={amount}
+          onChange={(e) => setAmount(parseInt(e.target.value))}
+          placeholder="Amount Per Time"
+          className="w-full"
+        />
+      </div>
+      <div className="gap-2">
+        <Label>Frequency</Label>
+        <Input
+          type="number"
+          value={frequency}
+          onChange={(e) => setFrequency(parseInt(e.target.value))}
+          placeholder="How often in days i.e. every 2 days..."
+          className="w-full"
+        />
+      </div>
+
+      <Button onClick={handleCreateSavingsPlan}>Create</Button>
+    </div>
+  );
 }
